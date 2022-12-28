86b5b62647b0dea18c3e6a3327a53f2dd74ca0d8
Sandro Covo <sandro@sandrocovo.ch>
2022-12-28 14:55:41 +0100

    style results a bit

--- rechner/forms.py
+++ rechner/forms.py
@@ -5,15 +5,9 @@ from rechner.models import KOSTENSTEIGERUNG
 
 class BasicRentCalculator(forms.Form):
     start_rent = forms.FloatField()
-    start_date = forms.DateField(widget=forms.DateInput(
-        format=('%Y-%m-%d'),
-        attrs={'type': 'date'},
-    ))
+    start_date = forms.DateField(widget=forms.DateInput(format=('%Y-%m-%d'), ))
 
-    new_date = forms.DateField(widget=forms.DateInput(
-        format=('%Y-%m-%d'),
-        attrs={'type': 'date'},
-    ))
+    new_date = forms.DateField(widget=forms.DateInput(format=('%Y-%m-%d'), ))
 
     cost_value = forms.FloatField()
     cost_type = forms.ChoiceField(choices=KOSTENSTEIGERUNG)
